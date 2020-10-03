df7088d991e3b71090e9e394485b136bf1815e47
Gregory Langlais <general@gregorylanglais.com>
2020-10-02 21:33:18 -0400

    fix word terminations on pig latin

--- ch08_03_hash_maps/src/language.rs
+++ ch08_03_hash_maps/src/language.rs
@@ -1,24 +1,14 @@
-// 1st consonant of each word is moved to end with "ay" added; "first" -> "irst-fay"
-// if word starts with vowel "hay" added instead; "apple" -> "apple-hay"
-// verify UTF encoding
 pub mod translate {
     pub fn to_pig_latin(text: &str) -> String {
         let vowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u'];
         let mut result = String::new();
         for word in text.split_whitespace() {
-            // consonant: first -> irst-fay
-            // consonant: t -> -tay
-            // vowel: oski -> oksi-hay
-            // vowel: i -> i-hay
-            // punctuation: hey! -> ey-hay!
-            // punctuation: hey!! -> ey-hay!!
-
             result.push(' ');
 
-            let mut chars = word.chars().peekable();
+            let mut chars = word.chars();
 
             // if word is single character push and finish
-            if chars.peek().is_some() {
+            if word.len() == 1 {
                 let char = chars.next().unwrap();
                 result.push(char);
                 continue;
@@ -30,7 +20,8 @@ pub mod translate {
             let mut suffix = String::new();
             let char = chars.next().unwrap();
             if vowels.contains(&char) {
-                suffix.push_str("-hay")
+                suffix.push_str("-hay");
+                result.push(char);
             } else if char.is_alphabetic() {
                 suffix.push_str(&format!("-{}ay", char));
             } else {
@@ -42,12 +33,15 @@ pub mod translate {
             // iteration remaining characters
             while let Some(char) = chars.next() {
                 if word_terminated {
-                    result.push(char);
+                    suffix.push(char);
                     continue
                 }
 
-                if !char.is_alphabetic() {
+                // non-alpha chars indicate word terminated
+                if !char.is_alphabetic() && char != '-' {
                     word_terminated = true;
+                    suffix.push(char);
+                    continue
                 }
 
                 result.push(char);
