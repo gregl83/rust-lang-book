d38dab75b24d3714cfe41fece1fb9a35b3526a2a
Gregory Langlais <general@gregorylanglais.com>
2020-10-19 17:05:38 -0500

    add lifetimes

---
+++ ch10_03_lifetime_syntax/src/main.rs
@@ -0,0 +1,15 @@
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() > b.len() {
+        a
+    } else {
+        b
+    }
+}
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
