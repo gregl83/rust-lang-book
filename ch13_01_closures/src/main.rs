468021016b15ee7c559381a935acf49bb3b0ae8f
Gregory Langlais <general@gregorylanglais.com>
2020-10-22 09:19:31 -0500

    add closure cacher

--- ch13_01_closures/src/main.rs
+++ ch13_01_closures/src/main.rs
@@ -1,3 +1,8 @@
+use ch13_01_closures::generate_workout;
+
 fn main() {
-    println!("Hello, world!");
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
 }
