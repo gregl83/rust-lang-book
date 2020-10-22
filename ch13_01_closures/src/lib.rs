650bbba893b2464c1dcc0013869411a42315863b
Gregory Langlais <general@gregorylanglais.com>
2020-10-22 12:29:16 -0500

    update closure to use hashmap

--- ch13_01_closures/src/lib.rs
+++ ch13_01_closures/src/lib.rs
@@ -1,12 +1,14 @@
 use std::thread;
 use std::time::Duration;
+use std::collections::HashMap;
+use std::borrow::Borrow;
 
 struct Cacher<T>
     where
         T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    value: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -16,19 +18,16 @@ impl<T> Cacher<T>
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            value: HashMap::new(),
         }
     }
 
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
+    fn value(&mut self, arg: u32) -> &mut u32 {
+        let value = self.value.entry(arg).or_insert(
+            (self.calculation)(arg)
+        );
+
+        value
     }
 }
 
@@ -57,5 +56,21 @@ pub fn generate_workout(intensity: u32, random_number: u32) {
                 expensive_result.value(intensity)
             );
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        assert_eq!(*v1, 1);
+
+        let v2 = c.value(2);
+        assert_eq!(*v2, 2);
     }
 }
