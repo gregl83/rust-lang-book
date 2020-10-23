2fc09a54541224119e5202f0cdbe962658f6531e
Gregory Langlais <general@gregorylanglais.com>
2020-10-23 13:59:01 -0400

    add crate publish metadata

--- ch14_02_publishing_to_crates_io/src/lib.rs
+++ ch14_02_publishing_to_crates_io/src/lib.rs
@@ -2,6 +2,11 @@
 //!
 //! using crates.io to document, test, and publish code
 
+// re-export for public use
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
 /// Returns one as unsigned 8 byte int
 ///
 /// # Examples
@@ -15,4 +20,32 @@
 /// ```
 pub fn one() -> u8 {
     1
+}
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    #[derive(Debug)]
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    #[derive(Debug)]
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        SecondaryColor::Green // fixme - whoops
+    }
 }
--- ch14_02_publishing_to_crates_io/src/main.rs
+++ ch14_02_publishing_to_crates_io/src/main.rs
@@ -1,7 +1,13 @@
-use ch14_02_publishing_to_crates_io::one;
+use ch14_02_publishing_to_crates_io::{PrimaryColor, mix, one};
 
 fn main() {
     let unsigned_int = one();
 
     println!("documented function: {}", unsigned_int);
+
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::Yellow;
+    let mixed = mix(red, yellow);
+
+    println!("{:?}", mixed);
 }
