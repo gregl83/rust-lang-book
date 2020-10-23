036297056126d9721baf39f41cca751d36644675
Gregory Langlais <general@gregorylanglais.com>
2020-10-23 13:36:37 -0400

    add crate comment docs

---
+++ ch14_02_publishing_to_crates_io/src/lib.rs
@@ -0,0 +1,14 @@
+/// Returns one as unsigned 8 byte int
+///
+/// # Examples
+///
+/// ```
+/// use self::ch14_02_publishing_to_crates_io::one;
+///
+/// let unsigned_int = one();
+///
+/// assert_eq!(1, unsigned_int);
+/// ```
+pub fn one() -> u8 {
+    1
+}
--- ch14_02_publishing_to_crates_io/src/main.rs
+++ ch14_02_publishing_to_crates_io/src/main.rs
@@ -1,3 +1,7 @@
+use ch14_02_publishing_to_crates_io::one;
+
 fn main() {
-    println!("Hello, world!");
+    let unsigned_int = one();
+
+    println!("documented function: {}", unsigned_int);
 }
