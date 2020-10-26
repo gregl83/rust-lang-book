141d506b294786ea5ad555cfeee386c457e9c396
Gregory Langlais <general@gregorylanglais.com>
2020-10-25 20:59:50 -0400

    add box list enum

--- ch15_01_box/src/main.rs
+++ ch15_01_box/src/main.rs
@@ -1,4 +1,16 @@
+use crate::List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, List),
+    Nil,
+}
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
+
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
+    println!("list = {:?}", list);
+    // fixme - infinite size of list
 }
