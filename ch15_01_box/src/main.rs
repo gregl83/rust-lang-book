d20b9295012c0403c795ba52458c99743e7a76f0
Gregory Langlais <general@gregorylanglais.com>
2020-10-26 22:17:01 -0400

    update box type completed

--- ch15_01_box/src/main.rs
+++ ch15_01_box/src/main.rs
@@ -2,7 +2,7 @@ use crate::List::{Cons, Nil};
 
 #[derive(Debug)]
 enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -10,7 +10,6 @@ fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
-    let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("list = {:?}", list);
-    // fixme - infinite size of list
 }
