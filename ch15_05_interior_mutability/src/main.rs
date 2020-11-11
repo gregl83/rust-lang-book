16953eb83b9cbdfeb34807436b3d6369af646756
Gregory Langlais <general@gregorylanglais.com>
2020-11-11 16:18:04 -0500

    add multiple owner (rc) interior mut

--- ch15_05_interior_mutability/src/main.rs
+++ ch15_05_interior_mutability/src/main.rs
@@ -1,3 +1,13 @@
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
 fn main() {
     let mut a = 1;
     let b = &a;
@@ -5,5 +15,16 @@ fn main() {
 
     println!("{}, {}, {}", a, b, c);
 
-    println!("Hello, world!");
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
 }
