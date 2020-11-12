7116de45188b2262506f332c4e84457bd25fd9fb
Gregory Langlais <general@gregorylanglais.com>
2020-11-12 14:25:31 -0500

    add reference cycle strong/weak counts

---
+++ ch15_06_reference_cycles/src/lib.rs
@@ -0,0 +1,25 @@
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
+#[derive(Debug)]
+pub enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    pub fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Node {
+    pub value: i32,
+    pub parent: RefCell<Weak<Node>>,
+    pub children: RefCell<Vec<Rc<Node>>>,
+}
