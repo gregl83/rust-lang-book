e9db765c8700525df8d86653bd07b89042ee31a1
Gregory Langlais <general@gregorylanglais.com>
2020-12-08 16:11:31 -0500

    add pattern refutability

--- ch18_01_all_the_places_for_patterns/src/main.rs
+++ ch18_01_all_the_places_for_patterns/src/main.rs
@@ -30,7 +30,36 @@ fn while_let_pattern() {
     }
 }
 
+fn for_loop_pattern() {
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{} is at index {}", value, index);
+    }
+}
+
+fn let_statements_pattern() {
+    // let PATTERN = EXPRESSION
+    let (x, y, z) = (1, 2, 3);
+    println!("{} {} {}", x, y, z);
+}
+
+fn function_pattern(&(x, y): &(i32, i32)) {
+    println!("Current location: ({}, {})", x, y);
+}
+
 fn main() {
+    // match VALUE {
+    //     PATTERN => EXPRESSION,
+    //     PATTERN => EXPRESSION,
+    //     PATTERN => EXPRESSION,
+    // }
+
     if_let_pattern();
     while_let_pattern();
+    for_loop_pattern();
+    let_statements_pattern();
+
+    let point = (3, 5);
+    function_pattern(&point);
 }
