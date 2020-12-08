46eb34614e6ab29c255350581f03d2cb7f474240
Gregory Langlais <general@gregorylanglais.com>
2020-12-08 16:53:31 -0500

    update refutability complete

--- ch18_02_refutability/src/main.rs
+++ ch18_02_refutability/src/main.rs
@@ -1,3 +1,18 @@
+fn refutable_pattern(some_option_value: Option<i32>) {
+    if let Some(x) = some_option_value {
+        println!("{}", x);
+    }
+}
+
+fn irrefutable_pattern(some_option_value: Option<i32>) {
+    match some_option_value {
+        Some(x) => println!("{}", x),
+        None => println!("{}", "none")
+    }
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    refutable_pattern(Some(1));
+    irrefutable_pattern(None);
 }
