944f3e88422ae29d8c52bf778bf84b810b2ee927
Gregory Langlais <general@gregorylanglais.com>
2020-12-10 11:31:31 -0500

    add unsafe raw pointers

--- ch19_01_unsafe_rust/src/main.rs
+++ ch19_01_unsafe_rust/src/main.rs
@@ -1,3 +1,22 @@
+fn deference_raw_pointer() {
+    let mut num = 5;
+
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+
+    unsafe {
+        println!("r1 is: {}", *r1);
+        println!("r2 is: {}", *r2);
+    }
+
+    let address = 0x012345usize;
+    let _r = address as *const i32;
+    // segmentation fault:
+    // unsafe {
+    //     println!("r is: {}", *_r);
+    // }
+}
+
 fn main() {
-    println!("Hello, world!");
+    deference_raw_pointer();
 }
