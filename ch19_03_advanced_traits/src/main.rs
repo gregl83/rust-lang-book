bf1e1dd192a6fe79533ca9384384d4ad6531c1b7
Gregory Langlais <general@gregorylanglais.com>
2020-12-10 15:12:27 -0500

    add trait types

--- ch19_03_advanced_traits/src/main.rs
+++ ch19_03_advanced_traits/src/main.rs
@@ -1,3 +1,23 @@
+trait Iterator {
+    type Item;
+
+    fn next(&mut  self) -> Option<Self::Item>;
+}
+
+#[derive(Debug)]
+struct Counter;
+
+impl Iterator for Counter {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // fixme - this doesn't iterate /shrug
+        Some(1)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let counter = Counter {};
+
+    println!("counter: {:?}", counter);
 }
