93e07ca609b2db8ffdbe0d1badcd97bfcf66d649
Gregory Langlais <general@gregorylanglais.com>
2020-12-10 15:31:59 -0500

    add overload default types

--- ch19_03_advanced_traits/src/main.rs
+++ ch19_03_advanced_traits/src/main.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 trait Iterator {
     type Item;
 
@@ -22,6 +24,30 @@ fn trait_types() {
     println!("counter: {:?}", counter);
 }
 
+#[derive(Debug)]
+struct Millimeters(u32);
+#[derive(Debug)]
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
+
+fn default_type_overload_overwrite() {
+    let mm = Millimeters(1);
+    let m = Meters(1);
+
+    let result = mm + m;
+
+    println!("mm + m = {:?}", result);
+}
+
 fn main() {
     trait_types();
+
+    default_type_overload_overwrite();
 }
