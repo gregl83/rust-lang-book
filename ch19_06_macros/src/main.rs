df2b34a89f81c62783d9019d7be3e393f6f095b6
Gregory Langlais <general@gregorylanglais.com>
2020-12-11 16:59:57 -0500

    add proc hello macro

--- ch19_06_macros/src/main.rs
+++ ch19_06_macros/src/main.rs
@@ -1,3 +1,35 @@
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[macro_export]
+macro_rules! myvec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
+
+fn declarative_macros() {
+    let v = myvec!(1, 2, 3);
+    println!("myvec macro: {:?}", v);
+}
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
+fn procedural_macros() {
+    Pancakes::hello_macro();
+}
+
 fn main() {
-    println!("Hello, world!");
+    declarative_macros();
+
+    // procedural macros (custom derive, attribute-like, and function-like)
+    // MUST reside in own crate
+    procedural_macros();
 }
