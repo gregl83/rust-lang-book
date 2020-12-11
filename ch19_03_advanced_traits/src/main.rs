82c7429d3d522076af27369c9049fbe7e36b5436
Gregory Langlais <general@gregorylanglais.com>
2020-12-11 13:25:19 -0500

    add fully qualified syntax

--- ch19_03_advanced_traits/src/main.rs
+++ ch19_03_advanced_traits/src/main.rs
@@ -46,8 +46,75 @@ fn default_type_overload_overwrite() {
     println!("mm + m = {:?}", result);
 }
 
+trait Pilot {
+    fn fly(&self);
+}
+
+trait Wizard {
+    fn fly(&self);
+}
+
+struct Human;
+
+impl Pilot for Human {
+    fn fly(&self) {
+        println!("This is your captain speaking.");
+    }
+}
+
+impl Wizard for Human {
+    fn fly(&self) {
+        println!("Up!");
+    }
+}
+
+impl Human {
+    fn fly(&self) {
+        println!("*waving arms furiously*");
+    }
+}
+
+// ----
+
+trait Animal {
+    fn baby_name() -> String;
+}
+
+struct Dog;
+
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
+    }
+}
+
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
+    }
+}
+
+fn fully_qualified_syntax() {
+    let person = Human;
+
+    Pilot::fly(&person);
+
+    Wizard::fly(&person);
+
+    person.fly();
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+
+    // fully qualified:
+    // <Type as Trait>::function(receiver_if_method, next_arg, ...);
+}
+
 fn main() {
     trait_types();
 
     default_type_overload_overwrite();
+
+    fully_qualified_syntax();
 }
